31dfa1452c929dc929dfab4e1214724005eba520
DaVv <davv09@gmail.com>
2020-04-24 11:50:09 +0200

    création du panier pour mettre en session les oeuvres selectionné par l'utilisateur

--- src/main/java/com/directmedia/onlinestore/core/entity/ShoppingCart.java
+++ src/main/java/com/directmedia/onlinestore/core/entity/ShoppingCart.java
@@ -1,12 +1,17 @@
 package com.directmedia.onlinestore.core.entity;
 
+
 import java.util.HashSet;
+import java.util.Set;
 
 public class ShoppingCart {
     //déclarations
-    private HashSet<Work> items=new HashSet<Work>();
-
-    public ShoppingCart() {
+    private Set<Work> items=new HashSet<Work>();
+    //Getter & setter
+    public Set<Work> getItems() {
+        return items;
     }
-
+    public void setItems(Set<Work> items) {
+        this.items = items;
+    }
 }
